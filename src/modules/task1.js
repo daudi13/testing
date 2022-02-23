273612bf7792f8bc3ec18cb0e48f2879f37263c0
daudi13 <davouma13@gmail.com>
2022-02-23 15:38:14 +0300

    Add refactor

--- src/modules/task1.js
+++ src/modules/task1.js
@@ -1,16 +1,11 @@
 
+const stringLength = str => str.length;
+const reverseString = str => str.split('').reverse().join('');
+const capitalize = str => str.toUpperCase();
 
 
-function stringLength(string) {
-	const str = string;
-	const strLength = str.split('').length;
-
-	if (strLength >= 1 && strLength <= 10) {
-		return strLength;
-	} else {
-		throw 'String exeeds 10 characters'
-	}
+module.exports = {
+	stringLength,
+	reverseString,
+	capitalize
 }
-
-
-module.exports = stringLength;
